00fa2586f6fb79f70ed1b17652419819e572aba3
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2022-02-06 14:16:46 -0300

    commit inicial

--- app/Http/Middleware/Tenant/TenantFilesystems.php
+++ app/Http/Middleware/Tenant/TenantFilesystems.php
@@ -16,11 +16,15 @@ class TenantFilesystems
      */
     public function handle(Request $request, Closure $next)
     {
-        $uuid = session('tenant')['uuid'];
-        config()->set([
-            'filesystems.disks.public.root' => config('filesystems.disks.public.root') . "/{$uuid}",
-            'filesystems.disks.public.url' => config('filesystems.disks.public.url') . "/{$uuid}",
-        ]);
+        
+        if(session('tenant')){
+            $uuid = session('tenant')['uuid'];
+            config()->set([
+                'filesystems.disks.public.root' => config('filesystems.disks.public.root') . "/{$uuid}",
+                'filesystems.disks.public.url' => config('filesystems.disks.public.url') . "/{$uuid}",
+            ]);
+        }
+
         return $next($request);
     }
 }
